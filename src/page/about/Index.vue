75a9b317c786bace7ea77eded6d9801820811c07
coding-by-feng <1215587087@qq.com>
2020-09-28 20:43:32 +0800

    Add my blog.

--- src/page/about/Index.vue
+++ src/page/about/Index.vue
@@ -10,25 +10,27 @@ export default {
 </script>
 
 <style>
-    .item {
-        padding: 18px 0;
-    }
+.item {
+  padding: 18px 0;
+}
 
-    .box-card {
-        width: 100%;
-    }
+.box-card {
+  width: 100%;
+}
 </style>
 
 <template>
-    <div>
-        <el-card class="box-card">
-            <div class="text item">
-                <p>作者wx：my_dream_it</p>
-                <p>作者邮箱：feng_orz@163.com</p>
-                <p>粤ICP备2020080500号</p>
-                <p style="color: red" v-show="true">温馨提示：浏览本网站请不要使用苹果自带的safari浏览器，需使用UC浏览器、谷歌浏览器等！</p>
-            </div>
-        </el-card>
-    </div>
+  <div>
+    <el-card class="box-card">
+      <div class="text item">
+        <p>作者博客：<a href="http://www.fengorz.me" style="color: #42b983" target="view_window">http://www.fengorz.me</a>
+        </p>
+        <p>作者微信：my_dream_it</p>
+        <p>作者邮箱：feng_orz@163.com</p>
+        <p>粤ICP备2020080500号</p>
+        <p style="color: red" v-show="true">温馨提示：浏览本网站请不要使用苹果自带的safari浏览器，需使用UC浏览器、谷歌浏览器等！</p>
+      </div>
+    </el-card>
+  </div>
 </template>
 
