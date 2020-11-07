5c3bcc2eb354299396ceff4f0d57de40f8e702fd
coding-by-feng <1215587087@qq.com>
2020-11-07 15:05:43 +0800

    Add QQ Group jumping.

--- src/page/about/Index.vue
+++ src/page/about/Index.vue
@@ -23,7 +23,7 @@ export default {
   <div>
     <el-card class="box-card">
       <div class="text item">
-        <img width="60%" src="@/assets/wx_gourp_scan_code.jpg">
+        <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=IdTwxPha3gHT5VNTbqokUjxQOMbTfsAP&jump_from=webapi"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="Kiwi词典QQ群" title="Kiwi词典QQ群"></a>
         <p>作者博客：<a href="http://www.fengorz.me" style="color: #42b983" target="view_window">http://fengorz.me</a></p>
         <p>作者微信：my_dream_it</p>
         <p>作者邮箱：feng_orz@163.com</p>
