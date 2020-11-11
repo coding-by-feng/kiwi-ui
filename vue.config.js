14649188248312c5e6291521818e4b510394e952
coding-by-feng <1215587087@qq.com>
2020-11-11 17:16:45 +0800

    Improve homepage access speed.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
 const url = 'http://kiwi-microservice-local:9991'
 // const url = 'http://localhost:9991'
 let publicPath = './'
@@ -14,6 +18,30 @@ module.exports = {
     entry
       .add('classlist-polyfill')
       .end()
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@i': path.resolve(__dirname, './src/assets'),
+      }
+    },
+    plugins: [
+      // Ignore all locale files of moment.js
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+      // 配置compression-webpack-plugin压缩
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5,
+        minChunkSize: 100
+      })
+    ]
   },
   devServer: {
     proxy: {
