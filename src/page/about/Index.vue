e55187423daa31d5c4da125470f0a980a64024bd
coding-by-feng <1215587087@qq.com>
2021-11-10 23:32:35 +0800

    Add play time of word spelling.

--- src/page/about/Index.vue
+++ src/page/about/Index.vue
@@ -24,7 +24,6 @@ export default {
     <el-card class="box-card">
       <div class="text item">
         <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=IdTwxPha3gHT5VNTbqokUjxQOMbTfsAP&jump_from=webapi"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="Kiwi词典QQ群" title="Kiwi词典QQ群"></a>
-        <p>粤ICP备2020080500号</p>
       </div>
     </el-card>
   </div>
