715f032d23be431cf5da411218e2675f3d8650cb
coding-by-feng <1215587087@qq.com>
2021-11-13 12:16:39 +0800

    Add my personal wechat QR code.

--- src/page/about/Index.vue
+++ src/page/about/Index.vue
@@ -25,6 +25,9 @@ export default {
       <div class="text item">
         <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=IdTwxPha3gHT5VNTbqokUjxQOMbTfsAP&jump_from=webapi"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="Kiwi词典QQ群" title="Kiwi词典QQ群"></a>
       </div>
+      <div class="text item">
+        <img src="wechat.png" alt="微信个人二维码">
+      </div>
     </el-card>
   </div>
 </template>
