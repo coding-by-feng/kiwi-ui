d4797c3a88d01ea41bee1e02a060748ffcb6877f
coding-by-feng <1215587087@qq.com>
2021-12-28 23:54:10 +0800

    Optimizing UI.

--- src/page/about/Index.vue
+++ src/page/about/Index.vue
@@ -22,12 +22,12 @@ export default {
 <template>
   <div>
     <el-card class="box-card">
-      <div class="text item">
+<!--      <div class="text item">
         <a target="_blank" href="https://www.fengorz.me/" style="color: #409EFF">click enter into my blog</a>
       </div>
       <div class="text item">
         <a target="_blank" href="https://github.com/coding-by-feng" style="color: #409EFF">click enter into my github</a>
-      </div>
+      </div>-->
       <div class="text item">
         <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=IdTwxPha3gHT5VNTbqokUjxQOMbTfsAP&jump_from=webapi"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="Kiwi词典QQ群" title="Kiwi词典QQ群"></a>
       </div>
