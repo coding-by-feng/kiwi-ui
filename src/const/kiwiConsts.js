9a42b34904f9e2d7a09b53c865189d6ca742b0ec
coding-by-feng <1215587087@qq.com>
2022-03-22 23:58:44 +0800

    Fix: Fix bug for review mode.

--- src/const/kiwiConsts.js
+++ src/const/kiwiConsts.js
@@ -1,9 +1,24 @@
 export default {
 
-  Review_Daily_Counter_Type: {
+  REVIEW_DAILY_COUNTER_TYPE: {
     REMEMBER: 1,
     KEEP_IN_MIND: 2,
     REVIEW: 3
+  },
+
+  /**
+   * 导播模式，是否附带中文导播
+   * 1：去除中文导播
+   * 2：附带中文导播
+   */
+  REVIEW_TYPE: {
+    ONLY_ENGLISH: 1,
+    WITH_CHINESE: 2,
+  },
+
+  ENGLISH_PARAPHRASE_TYPE: {
+    DISABLE: 1,
+    ENABLE: 2
   }
 
 }
