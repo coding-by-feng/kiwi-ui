0b5592608b2cc12711bf4842687c06b2aabf4231
coding-by-feng <1215587087@qq.com>
2022-08-27 11:49:22 +0800

    refactor(TTS): Deprecate TTS api key related function.

--- src/const/kiwiConsts.js
+++ src/const/kiwiConsts.js
@@ -4,8 +4,6 @@ export default {
         VOICE_RSS: 'https://api.voicerss.org'
     },
 
-    DEFAULT_MAX_REVIEW_COUNT_FOR_VOICE_RSS: 350,
-
     PRONUNCIATION_SOURCE: {
         CAMBRIDGE: 'Cambridge',
         LOCAL: 'Local'
@@ -76,22 +74,6 @@ export default {
         .set('plural', '名词复数形式')
         .set('preposition', '介词或者前置词')
         .set('phrase', '短语'),
-
-    API_KEY_VOICE_RSS: {
-        AUTO: '自动选择',
-        KEY1: '02df0a8f48b641548ec4224c24ebff0e',
-        KEY2: '0e3c0a35570543249f743f74c027ef8b',
-        KEY3: 'a65b84ea89b14011af581b3335e40d63',
-        KEY4: '587527ea30b44778b6bbca2bcac95f38',
-        KEY5: '664f011d4efc4937b4d75a8b962f0323',
-        KEY6: '59a1e3301e4943edaab90166e3c1aafb',
-        KEY7: '58d4baef52414088998cbbda9751c8e2'
-    },
-
-    CACHE_KEY: {
-        TT_API_KEY: 'tts_api_key',
-        TTS_CURRENT_API_KEY: 'tts_current_api_key'
-    },
 
     REVIEW_AUDIO_TYPE: {
         WORD_SPELLING: 0,
