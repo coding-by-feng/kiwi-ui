a6193b26a23794576af11085a2cdd3bde7529d53
coding-by-feng <1215587087@qq.com>
2022-10-04 14:19:38 +0800

    refactor(*): Add optimization for the Ch2En revise mode.

--- src/util/howlerUtil.js
+++ src/util/howlerUtil.js
@@ -1,8 +1,10 @@
 import kiwiConst from '@/const/kiwiConsts'
 
+let CH2_EN_INDEX_SLEEP_MS_MAP = null
+
 export default {
 
-    extractedUrls: function (lastIsSame, paraphraseId, wordId, ukPronunciationUrl, usPronunciationUrl, characterCode, exampleList) {
+    extractedEn2ChUrls: function (lastIsSame, paraphraseId, wordId, ukPronunciationUrl, usPronunciationUrl, characterCode, exampleList) {
         let urls = []
         urls.push(ukPronunciationUrl)
         urls.push(usPronunciationUrl)
@@ -24,6 +26,54 @@ export default {
         urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_EN))
         urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_EN))
         urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_CH))
+        urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_EN))
+        if (!exampleList || exampleList.length === 0) {
+            return urls;
+        }
+        if (exampleList.length === 1) {
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_CH));
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_CH));
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_EN));
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_EN));
+        } else {
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_CH));
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_CH));
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_EN));
+            urls.push(this.assembleReviseAudioUrl(exampleList[0].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_EN));
+            urls.push(this.assembleReviseAudioUrl(exampleList[1].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_CH));
+            urls.push(this.assembleReviseAudioUrl(exampleList[1].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_CH));
+            urls.push(this.assembleReviseAudioUrl(exampleList[1].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_EN));
+            urls.push(this.assembleReviseAudioUrl(exampleList[1].exampleId, kiwiConst.REVIEW_AUDIO_TYPE.EXAMPLE_EN));
+        }
+        return urls;
+    },
+
+    acquireCh2EnIndexSleepMsMap() {
+        if (CH2_EN_INDEX_SLEEP_MS_MAP === null) {
+            CH2_EN_INDEX_SLEEP_MS_MAP = new Map()
+                .set(0, 2000)
+                .set(1, 2000)
+        }
+        return CH2_EN_INDEX_SLEEP_MS_MAP;
+    },
+
+    extractedCh2EnUrls: function (paraphraseId, wordId, ukPronunciationUrl, usPronunciationUrl, characterCode, exampleList) {
+        let urls = []
+        urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_CH))
+        urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_CH))
+        urls.push(this.assembleCharacterReviseAudioUrl(characterCode))
+        urls.push(ukPronunciationUrl)
+        urls.push(usPronunciationUrl)
+        urls.push(this.assembleReviseAudioUrl(wordId, kiwiConst.REVIEW_AUDIO_TYPE.WORD_SPELLING));
+        urls.push(this.assembleReviseAudioUrl(wordId, kiwiConst.REVIEW_AUDIO_TYPE.WORD_SPELLING))
+        urls.push(this.assembleReviseAudioUrl(wordId, kiwiConst.REVIEW_AUDIO_TYPE.WORD_SPELLING))
+        urls.push(ukPronunciationUrl)
+        urls.push(usPronunciationUrl)
+        urls.push(this.assembleReviseAudioUrl(wordId, kiwiConst.REVIEW_AUDIO_TYPE.WORD_SPELLING))
+        urls.push(this.assembleReviseAudioUrl(wordId, kiwiConst.REVIEW_AUDIO_TYPE.WORD_SPELLING))
+        urls.push(this.assembleCharacterReviseAudioUrl(characterCode))
+        urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_CH))
+        urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_EN))
         urls.push(this.assembleReviseAudioUrl(paraphraseId, kiwiConst.REVIEW_AUDIO_TYPE.PARAPHRASE_EN))
         if (!exampleList || exampleList.length === 0) {
             return urls;
